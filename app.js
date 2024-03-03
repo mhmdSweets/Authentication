d5b9007ae8c7e27ac4af379f7b54a32f9955cd87
Mhmd Sweets <you@example.com>
2024-03-03 20:24:53 +0200

    Added Level 6 - OAuth 2.0 & How to Implement Sign In with Google

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const saltRounds = 10;
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 // creating express app
 const app = express();
@@ -24,6 +26,7 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
+    cookie: { secure: true },
   })
 );
 // passport
@@ -36,21 +39,111 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSechema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
 });
 
-// using passport-local-mongoose
+// using passport-local-mongoose & findOrCreate plugin
 userSechema.plugin(passportLocalMongoose);
+userSechema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSechema);
 // passport config
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
+});
 
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+//
+// passport google
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function verify(issuer, profile, cb) {
+      db.get(
+        "SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?",
+        [issuer, profile.id],
+        function (err, row) {
+          if (err) {
+            return cb(err);
+          }
+          if (!row) {
+            db.run(
+              "INSERT INTO users (name) VALUES (?)",
+              [profile.displayName],
+              function (err) {
+                if (err) {
+                  return cb(err);
+                }
+
+                var id = this.lastID;
+                db.run(
+                  "INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)",
+                  [id, issuer, profile.id],
+                  function (err) {
+                    if (err) {
+                      return cb(err);
+                    }
+                    var user = {
+                      id: id,
+                      name: profile.displayName,
+                    };
+                    return cb(null, user);
+                  }
+                );
+              }
+            );
+          } else {
+            db.get(
+              "SELECT * FROM users WHERE id = ?",
+              [row.user_id],
+              function (err, row) {
+                if (err) {
+                  return cb(err);
+                }
+                if (!row) {
+                  return cb(null, false);
+                }
+                return cb(null, row);
+              }
+            );
+          }
+        }
+      );
+    }
+  )
+);
+
+//
 // Gets
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/auth/google", (req, res) => {
+  passport.authenticate("google", { scope: ["profile"] });
+});
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  }
+);
+
 app.get("/login", (req, res) => {
   res.render("login");
 });
@@ -60,6 +153,13 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/");
+  }
+});
+app.get("/submit", (req, res) => {
   if (req.isAuthenticated()) {
     res.render("secrets");
   } else {
